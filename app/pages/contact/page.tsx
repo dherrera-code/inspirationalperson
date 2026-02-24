16e02616365b4fec73de7d6b2e0cb390cf82e6c4
dherrera-code <dherr929@gmail.com>
2026-02-24 11:48:28 -0800

    Initialize repo for my inspirational little sister website!

---
+++ app/pages/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1 className='text-black'>Insert Contact me page content here!</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ app/pages/why/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Why = () => {
+  return (
+    <div>
+        <h1 className='text-black'>Insert Personal Impact content here!</h1>
+    </div>
+  )
+}
+
+export default Why
