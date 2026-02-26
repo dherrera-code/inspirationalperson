7ec3dce6a036b85d0012be277ae30ba744323634
dherrera-code <dherr929@gmail.com>
2026-02-25 16:08:15 -0800

    adjust responsiveness in the home page to have horizontal padding!

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
         {/* Hero Image */}
         <div className="bg-[url(/assets/hero-bg.jpg)] h-[410px] bg-center"></div>
 
-        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 py-15 sm:px-[200px]">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(220px,_1fr))] gap-5 py-15 px-4 sm:px-[200px]">
             <div>
                   <h1 className="font-title text-[40px] pb-5">Vanessa Bravo</h1>
                   <p>Vanessa is my adorable and successful little sister who in her actions motivates and inspires me to work hard to better my life and to those around me! </p>
             </div>
             <div className="place-items-center lg:place-items-end">
-              <img className="h-[260px]" src="/assets/vanessaSnow.jpeg" alt="A day in the snow with my sister Vanessa!" />
+              <img className="h-[300px]" src="/assets/vanessaSnow.jpeg" alt="A day in the snow with my sister Vanessa!" />
             </div>
         </div>
       </main>
