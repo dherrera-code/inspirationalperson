a850213dbe12273ad1057b6e426de7f4ff45659d
dherrera-code <dherr929@gmail.com>
2026-03-10 15:28:24 -0700

    update classes to match light mode with dark mode!

--- app/components/BottomFooter.tsx
+++ app/components/BottomFooter.tsx
@@ -2,17 +2,17 @@ import { Footer, FooterCopyright, FooterDivider, FooterLink, FooterLinkGroup } f
 
 const BottomFooter = () => {
   return (
-    <Footer container className='rounded-none dark:bg-[#9CA3AF]'>
+    <Footer container className='rounded-none bg-[#9CA3AF] dark:bg-[#9CA3AF] text-shadow-white'>
       <div className="w-full text-center ">
         <div className="w-full justify-between sm:flex sm:items-center sm:justify-between">
-          <h1 className='font-title text-[26px]'>A Life That Inspired Mine</h1>
-          <FooterLinkGroup>
+          <h1 className='font-title text-[26px] text-white'>A Life That Inspired Mine</h1>
+          <FooterLinkGroup className='text-white'>
             <FooterLink className='font-actor md:me-2 text-[20px]' href="/pages/about">About</FooterLink>
             <FooterLink className='font-actor md:me-2 text-[20px]' href="/pages/contact">Contact</FooterLink>
           </FooterLinkGroup>
         </div>
         <FooterDivider />
-        <FooterCopyright className='dark:text-white' by="Daniel Herrera™" year={2026} />
+        <FooterCopyright className='text-white dark:text-white' by="Daniel Herrera™" year={2026} />
       </div>
     </Footer>
   )
--- app/components/TopNavbar.tsx
+++ app/components/TopNavbar.tsx
@@ -14,10 +14,10 @@ const TopNavbar = () => {
         return currentPath === path;
     }
     return (
-        <header>
-            <Navbar fluid rounded className="py-6">
+        <header className="text-white">
+            <Navbar fluid rounded className="py-6 border-gray-700 bg-gray-800 text-gray-400 hover:bg-gray-700 hover:text-white md:hover:bg-transparent md:hover:text-white">
                 <NavbarBrand>
-                    <span className="self-center whitespace-nowrap text-[26px] font-title dark:text-white ps-1.5">A Life That Inspired Mine</span>
+                    <span className="self-center whitespace-nowrap text-[26px] font-title dark:text-white text-white ps-1.5">A Life That Inspired Mine</span>
                 </NavbarBrand>
                 <NavbarToggle />
                 <NavbarCollapse>
@@ -25,10 +25,10 @@ const TopNavbar = () => {
                     {/* {pages.map((page, index) => (
                         <NavbarLink key={index} className={isActive(page.path) ? 'active md:me-2 font-actor text-[20px]' : "md:me-2 font-actor text-[20px]"} href={page.path} >{page.name}</NavbarLink>
                     ))} */} 
-                    <NavbarLink className={isActive("/") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px]"} href={pages[0].path}>{pages[0].name}</NavbarLink>
-                    <NavbarLink  className={isActive("/pages/about") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px]"} href={pages[1].path}>{pages[1].name}</NavbarLink>
-                    <NavbarLink  className={isActive("/pages/why") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px]"} href={pages[2].path}>{pages[2].name}</NavbarLink>
-                    <NavbarLink  className={isActive("/pages/contact") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px]"} href={pages[3].path}>{pages[3].name}</NavbarLink>
+                    <NavbarLink className={isActive("/") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px] text-gray-400"} href={pages[0].path}>{pages[0].name}</NavbarLink>
+                    <NavbarLink  className={isActive("/pages/about") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px] text-gray-400"} href={pages[1].path}>{pages[1].name}</NavbarLink>
+                    <NavbarLink  className={isActive("/pages/why") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px] text-gray-400"} href={pages[2].path}>{pages[2].name}</NavbarLink>
+                    <NavbarLink  className={isActive("/pages/contact") ? "active md:me-1.5 font-actor text-[18px]" : "md:me-1.5 font-actor text-[18px] text-gray-400"} href={pages[3].path}>{pages[3].name}</NavbarLink>
                 </NavbarCollapse>
             </Navbar>
         </header>
